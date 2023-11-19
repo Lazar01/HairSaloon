967a8d411bd21c5fedb32d976e795d6a5d98fce5
Lazar <lekoviclazar0@gmail.com>
2023-11-19 09:52:09 +0100

    Applying style changes to home page.

--- frontend/routes/HomePage.tsx
+++ frontend/routes/HomePage.tsx
@@ -1,7 +1,20 @@
 function HomePage(){
     return(
-        <div>
+        <div className="flex flex-col h-screen">
+                <h2 className="mt-52 text-6xl text-center text-white font-extrabold leading-none tracking-tight  md:text-9xl dark:text-white drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)]">Hair Barber</h2>
+                <p className="pl-52 pr-52 text-justify md:text-lg text-xl text-white bg-gradient-to-b from-transparent via-slate-400 to-black h-full mt-24 flex items-center bg-opacity-90">
+                Meet [Barber's Name], your go-to expert for all things hair at [Barbershop Name]. With a passion for the art of hair styling that's as sharp as his scissors, [Barber's Name] is a true maestro when it comes to transforming locks into works of art.
 
+                [Barber's Name] brings years of experience to the barber's chair, making each haircut an unforgettable experience. His skillful hands can craft classic looks that stand the test of time, or he can push the boundaries with avant-garde styles that are on the cutting edge of fashion.
+
+                Customer satisfaction is [Barber's Name]'s top priority. He takes the time to listen to your desires and offers expert advice on what style suits you best, ensuring that you leave the chair feeling not only rejuvenated but also confident in your new look.
+
+                Beyond his technical prowess, [Barber's Name] is known for his warm and welcoming demeanor. His chair isn't just a place to get your hair done; it's a space for relaxation and conversation, where you can unwind while [Barber's Name] works his magic.
+
+                From classic scissor cuts to modern fades, beard trims to hot towel shaves, [Barber's Name] does it all with precision and style. Whether you're looking for a quick touch-up or a complete hair transformation, you can trust [Barber's Name] to deliver results that exceed your expectations.
+
+                Book your appointment with [Barber's Name] at [Barbershop Name] today and discover why he's the go-to choice for those who demand the best in hair care and styling. Elevate your look and experience the artistry of a true master barber.
+                </p>
         </div>
     );
 }
