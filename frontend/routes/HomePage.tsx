ae683d971bc97037a20df6c7735469f56476d01b
Lazar Lekovic <lekoviclazar0@gmail.com>
2023-12-18 22:28:26 +0100

    Added one more card on home page. Changed colors on home page

--- frontend/routes/HomePage.tsx
+++ frontend/routes/HomePage.tsx
@@ -15,7 +15,7 @@ function HomePage(){
     setShowModal((prevShowModal) => !prevShowModal);
   };
     return(
-        <div className="flex flex-col bg-white bg-opacity-90">
+        <div className="flex flex-col bg-white">
             <h2 className="pt-6 text-6xl text-center text-black font-extrabold leading-none tracking-tight sm:text-6xl  md:text-8xl drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)]">Hair Barber</h2>
                 <div className="flex pt-6 sm:mt-12 md:mt-0   flex-col md:flex-row justify-center gap-x-12 sm:gap-y-12 md:gap-y-0 items-center">
                     <Card className="w-96 bg-black">
@@ -48,7 +48,43 @@ function HomePage(){
                             onClick={() => setShowModal(!showModal)}
                             ripple={false}
                             fullWidth={true}
-                            className="bg-blue-gray-900/10 text-blue-gray-200 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                            className="bg-white text-black shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                            >
+                            Make appointment
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                    <Card className="w-96 bg-black">
+                        <CardHeader shadow={false} floated={false} className="sm:h-60 md:h-64">
+                            <img
+                            src="../assets/homePageImg1.jpg"
+                            alt="card-image"
+                            className="h-full w-full object-cover"
+                            />
+                        </CardHeader>
+                        <CardBody>
+                            <div className="mb-2 flex items-center justify-between">
+                            <Typography color="blue" className="font-medium">
+                            A clean angled haircut
+                            </Typography>
+                            <Typography color="blue" className="font-medium">
+                                $115.00
+                            </Typography>
+                            </div>
+                            <Typography
+                            variant="small"
+                            color="white"
+                            className="font-normal opacity-75"
+                            >
+                            A clean angled haircut features a precise and defined look with hair cut at an angle, creating sharp lines and a polished appearance.
+                            </Typography>
+                        </CardBody>
+                        <CardFooter className="pt-0">
+                            <Button
+                            onClick={() => setShowModal(!showModal)}
+                            ripple={false}
+                            fullWidth={true}
+                            className="bg-white text-black shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
                             >
                             Make appointment
                             </Button>
@@ -85,7 +121,7 @@ function HomePage(){
                             onClick={() => setShowModal(!showModal)}
                             ripple={false}
                             fullWidth={true}
-                            className="bg-blue-gray-900/10 text-blue-gray-200 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                            className="bg-white text-black shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
                             >
                             Make appointment
                             </Button>
@@ -93,12 +129,12 @@ function HomePage(){
                     </Card>
                 </div>
                 
-                <h1 className="block font-sans pt-12 sm:text-3xl md:text-5xl antialiased font-semibold leading-tight tracking-normal text-inherit text-center mt-12 bg-black text-white">
+                <h1 className="block font-sans pt-12 sm:text-3xl md:text-5xl antialiased font-semibold leading-tight tracking-normal text-inherit text-center mt-12 bg-white text-black">
                  About us
                 </h1>
 
 
-                <p className="pl-6 pr-6 pt-12 pb-12 md:pl-28 md:pr-28 md:text-xl text-md text-white font-semibold bg-black h-full flex items-center text-justify">
+                <p className="pl-6 pr-6 pt-12 pb-12 md:pl-28 md:pr-28 md:text-xl text-md text-black font-semibold bg-white h-full flex items-center text-justify">
 
                 Meet Lazar, your go-to expert for all things hair at Hair Barber. With a passion for the art of hair styling that's as sharp as his scissors, Lazar is a true maestro when it comes to transforming locks into works of art.
 
