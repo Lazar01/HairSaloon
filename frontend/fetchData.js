250a953a64c0a90c0249b9ac3d58cc30cebd2b27
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-01-22 21:22:58 +0100

    Changed the url for getting the employees

--- frontend/fetchData.js
+++ frontend/fetchData.js
@@ -3,7 +3,7 @@ import {useLazyAxios} from "use-axios-client";
 
 export function getAllEmployees() {
   const { data, error, loading } = useAxios({
-    url: "http://localhost:3000/getAppointments",
+    url: "http://localhost:3000/getAllEmployees",
     method: "GET",
   });
   return { data, error, loading };
