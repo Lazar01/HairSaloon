7dda5a22312b62580757948e17500e782532a305
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-01-31 16:01:42 +0100

    Made code more readable.

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const app = express();
-
-
 const cors = require('cors');
-
-
-const appRoute = require('./routes/route.js')
+const appRoute = require('./routes/route.js');
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(appRoute);
-
-
-
-
-
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
     console.log("Server is running on port: " + port)
