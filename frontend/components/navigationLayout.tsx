22e1abeb8271fe4ba87eb1295eb4464c7e1bb49a
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-02-06 22:46:47 +0100

    Changed the logo in navbar

--- frontend/components/navigationLayout.tsx
+++ frontend/components/navigationLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu, GiBeard } from "react-icons/gi";
 import { IconButton } from "@material-tailwind/react";
 import { useState } from "react";
 import headerBackground from "../assets/barberHeaderBackground.jpg"
@@ -10,9 +10,7 @@ function NavigationLayout() {
     <>
     <nav className="bg-black bg-opacity-75 bg-blend-overlay flex justify-between bg-center" style={{ backgroundImage: `url(${headerBackground})` }}>
       <Link to="/" className="mt-auto mb-auto pl-12 flex md:h-24 sm:h-12 items-center justify-center text-white font-extrabold leading-none tracking-tight  md:text-2xl drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)] basis-2/12">
-        <div className="">Barber Saloon   
-          <img className="sm:h-6 md:h-10 ml-auto mr-auto" src="../assets/Logo.png"></img>  
-        </div>
+          <GiBeard className="sm:h-6 sm:w-6 md:h-16 md:w-16 ml-auto mr-auto text-white"/>
       </Link>
       <ul className= {`${isMenuOpen?"block" : "hidden"} justify-center md:items-center md:basis-8/12 md:space-x-4 md:flex`}>
         <li className="p-2  text-lg md:text-2xl text-white hover:transform hover:scale-110 hover:animate-pulse drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)] focus:underline">         
