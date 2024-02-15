851826839f4e69492127e7823458d1b3ffadd65e
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-02-15 19:36:21 +0100

    Changed index page to be login, added new Route for register.

--- frontend/components/navigationLayout.tsx
+++ frontend/components/navigationLayout.tsx
@@ -9,12 +9,12 @@ function NavigationLayout() {
   return (
     <>
     <nav className="bg-black bg-opacity-75 bg-blend-overlay flex justify-between bg-center" style={{ backgroundImage: `url(${headerBackground})` }}>
-      <Link to="/" className="mt-auto mb-auto pl-12 flex md:h-24 sm:h-12 items-center justify-center text-white font-extrabold leading-none tracking-tight  md:text-2xl drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)] basis-2/12">
+      <Link to="/home" className="mt-auto mb-auto pl-12 flex md:h-24 sm:h-12 items-center justify-center text-white font-extrabold leading-none tracking-tight  md:text-2xl drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)] basis-2/12">
           <GiBeard className="sm:h-6 sm:w-6 md:h-16 md:w-16 ml-auto mr-auto text-white"/>
       </Link>
       <ul className= {`${isMenuOpen?"block" : "hidden"} justify-center md:items-center md:basis-8/12 md:space-x-4 md:flex`}>
         <li className="p-2  text-lg md:text-2xl text-white hover:transform hover:scale-110 hover:animate-pulse drop-shadow-[0_1.6px_1.6px_rgba(0,0,0,1)] focus:underline">         
-          <Link to="/">
+          <Link to="/home">
             <span className="">Home</span>
           </Link>
         </li>
@@ -46,7 +46,10 @@ function NavigationLayout() {
             <GiHamburgerMenu />
           </IconButton>
         </div>
-        Welcome</div>
+          <Link to="/">
+            Login/Register
+          </Link>
+        </div>
     </nav>
     <Outlet />
     </>
