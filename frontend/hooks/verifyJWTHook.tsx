ce2a14b5a909f94622b2123eef98d79a0b971c73
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-02-19 17:03:57 +0100

    Added role as field in User interface. Setting the expire time on token when user is authenticated

--- frontend/hooks/verifyJWTHook.tsx
+++ frontend/hooks/verifyJWTHook.tsx
@@ -7,10 +7,11 @@ interface User {
   EmailAddress: string;
   ContactNumber: string;
   Password: string;
+  Role: string;
 }
 
 const useVerifyAuthentication = () => {
-  const { data, refetch, error } = AuthenticateJWT();
+  const { data, refetch, error, loading } = AuthenticateJWT();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState<User>({
     CustomerID: -1,
@@ -18,17 +19,21 @@ const useVerifyAuthentication = () => {
     EmailAddress: "",
     ContactNumber: "",
     Password: "",
+    Role: "",
   });
+  const [expTime, setExpTime] = useState(0);
+  console.log(data);
   useEffect(() => {
     if (data && data.Message === "Authenticated") {
       setIsAuthenticated(true);
       setUser(data.user);
+      setExpTime(data.ExpTime);
     } else {
       setIsAuthenticated(false);
     }
   }, [data]);
 
-  return { isAuthenticated, user, refetch };
+  return { isAuthenticated, user, refetch, expTime, error, loading };
 };
 
 export default useVerifyAuthentication;
