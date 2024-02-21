071318040056825651bf9a12d11923199a354743
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-02-22 00:46:05 +0100

    On clicking edit button of a card opens a modal for editing clicked card.

--- frontend/components/blogCard.jsx
+++ frontend/components/blogCard.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { MdDelete, MdEdit } from "react-icons/md";
-import { Button, IconButton, Input } from "@material-tailwind/react";
+import { IconButton } from "@material-tailwind/react";
+import EditBlog from "../components/editBlogModal";
 const BlogCard = ({
   image,
+  id,
   date,
   CardTitle,
   CardDescription,
   isAuthenticated,
   role,
+  refetch,
 }) => {
   const [EditMode, setEditMode] = useState(false);
   function handleEdit(mode, event) {
@@ -16,45 +19,53 @@ const BlogCard = ({
   }
   console.log(EditMode);
   return (
-    <>
-      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-        <div className="mb-10 w-full">
-          <div className="relative mb-8 overflow-hidden rounded">
-            {isAuthenticated && role && (
-              <div className="absolute top-0 left-0 m-2">
-                <IconButton onClick={(e) => handleEdit(!EditMode, e)}>
-                  <MdEdit />
-                </IconButton>
-              </div>
-            )}
-            {isAuthenticated && role && (
-              <div className="absolute top-0 right-0 m-2">
-                <IconButton onClick={(e) => e.stopPropagation()}>
-                  <MdDelete />
-                </IconButton>
-              </div>
-            )}
-            <img src="../assets/service1.jpg" alt="" className="w-full h-80" />
-          </div>
+    <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+      <div className="mb-10 w-full">
+        <div className="relative mb-8 overflow-hidden rounded">
+          {isAuthenticated && role && (
+            <div className="absolute top-0 left-0 m-2">
+              <IconButton onClick={(e) => handleEdit(!EditMode, e)}>
+                <MdEdit />
+              </IconButton>
+            </div>
+          )}
+          {isAuthenticated && role && (
+            <div className="absolute top-0 right-0 m-2">
+              <IconButton onClick={(e) => e.stopPropagation()}>
+                <MdDelete />
+              </IconButton>
+            </div>
+          )}
+          <img src="../assets/service1.jpg" alt="" className="w-full h-80" />
+        </div>
 
-          <div>
-            {date && (
-              <span className="mb-5 inline-block rounded bg-primary px-4 py-1 text-center text-xs font-semibold leading-loose text-white">
-                {date}
-              </span>
-            )}
-            <h3>
-              <span className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                {CardTitle}
-              </span>
-            </h3>
-            <p className="text-base text-body-color dark:text-dark-6">
-              {CardDescription}
-            </p>
-          </div>
+        <div>
+          {date && (
+            <span className="mb-5 inline-block rounded bg-primary px-4 py-1 text-center text-xs font-semibold leading-loose">
+              {date}
+            </span>
+          )}
+          <h3>
+            <span className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl">
+              {CardTitle}
+            </span>
+          </h3>
+          <p className="text-base text-body-color dark:text-dark-6">
+            {CardDescription}
+          </p>
         </div>
       </div>
-    </>
+      {EditMode && (
+        <EditBlog
+          title={CardTitle}
+          desc={CardDescription}
+          image="../assets/service1.jpg"
+          setShowModal={setEditMode}
+          id={id}
+          refetch={refetch}
+        />
+      )}
+    </div>
   );
 };
 export default BlogCard;
