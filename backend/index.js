a9e314a68fb19d51a8db0df7d7ad0fb19b6a45c9
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-02-22 20:02:50 +0100

    Formatting

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const appRoute = require('./routes/route.js');
+const cors = require("cors");
+const appRoute = require("./routes/route.js");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(appRoute);
 
-app.listen(port, ()=>{
-    console.log("Server is running on port: " + port)
-})
+app.listen(port, () => {
+  console.log("Server is running on port: " + port);
+});
