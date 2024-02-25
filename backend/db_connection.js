8e1f049df89310e6e2aed1e4dea8be0fb6c2b9a7
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-02-25 22:53:23 +0100

    Format and added no addition to time zone for db.

--- backend/db_connection.js
+++ backend/db_connection.js
@@ -1,21 +1,21 @@
-const dotenv = require('dotenv');
-dotenv.config({path: './.env'})
-function connectWithDatabase()
-{
-    var mysql = require('mysql');
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+function connectWithDatabase() {
+  var mysql = require("mysql");
 
-    var con = mysql.createConnection({
+  var con = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-    });
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-      });
+    database: process.env.DATABASE,
+    timezone: "+00:00",
+  });
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
-      return con;
+  return con;
 }
 
-module.exports = {connectWithDatabase};
+module.exports = { connectWithDatabase };
