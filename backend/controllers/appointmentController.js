d92cefe9734448379def33acbdb75c2c45a6c820
Lazar Lekovic <lekoviclazar0@gmail.com>
2024-03-13 15:56:48 +0100

    Changed logic of getAppointments function to send back appointments instead of time.

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -30,46 +30,26 @@ function insertAppointment(req, res) {
 }
 
 const getAppointments = async (req, res) => {
-  try {
-    const { EmployeeID, Date } = req.query;
-    const query = `SELECT * FROM appointments WHERE EmployeeID = ? AND Date = ?`;
-    const values = [EmployeeID, Date];
-    console.log(req.query);
-    await db.query(query, values, (err, result) => {
-      const time = [
-        "09:00",
-        "09:30",
-        "10:00",
-        "10:30",
-        "11:00",
-        "11:30",
-        "12:00",
-        "12:30",
-        "13:00",
-        "13:30",
-        "14:00",
-        "14:30",
-        "15:00",
-        "15:30",
-        "16:00",
-        "16:30",
-        "17:00",
-      ];
-      const reservedTimes = result.map((appointment) =>
-        appointment.Time.split(":").slice(0, 2).join(":")
-      );
-      const responseData = time.filter((item) => !reservedTimes.includes(item));
-      return res.status(200).json(responseData);
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json(error);
-  }
+  const { CustomerID, UserRole } = req.query;
+  let query = `SELECT customer.Name,Date,Time, employee.Name as BarberName FROM appointments INNER JOIN employee ON employee.EmployeeID = appointments.EmployeeID INNER JOIN customer ON appointments.CustomerID = customer.CustomerID WHERE Date>current_date() AND appointments.CustomerID = ? ;`;
+  if (UserRole == "admin")
+    query =
+      "SELECT customer.Name,Date,Time, employee.Name as BarberName FROM appointments INNER JOIN employee ON employee.EmployeeID = appointments.EmployeeID INNER JOIN customer ON appointments.CustomerID = customer.CustomerID WHERE Date>current_date()";
+  const values = [CustomerID];
+  await db.query(query, values, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error while retrieving appointments");
+    }
+    console.log("Succecsfully retrieved appointments");
+    return res.status(200).json(result);
+  });
 };
 
-const makeAppointment = (req, res) => {
+const makeAppointment = async (req, res) => {
   const token = req.headers["access-token"];
-  const verify = userVerification(token);
+  const verify = await userVerification(token);
+  console.log(verify);
   if (!verify.bool) {
     return res.send("NotAuthenticated");
   } else {
@@ -81,13 +61,13 @@ const makeAppointment = (req, res) => {
     const currentDate = new Date();
     console.log(reqDate);
     const query_DateRestriction =
-      "SELECT max(date) as latestDate, max(time) as time FROM appointments WHERE customerid=16";
+      "SELECT max(date) as latestDate, max(time) as time FROM appointments WHERE customerid=?";
     const DateRestrictionValues = req.body.customerID;
     db.query(query_DateRestriction, DateRestrictionValues, (err, data) => {
       if (err) {
         console.log(err);
         return res.status(500).send("Cant query dates from appointments!");
-      } else if (data.length > 0) {
+      } else if (data.latestDate) {
         const [hours, minutes, seconds] = data[0].time.split(":");
 
         const AppointmentDate = new Date(data[0].latestDate);
